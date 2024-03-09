53d9dd465e51739394c2cb3916ae6cfbeaa17bb9
Андрей <chosenfew@yandex.ru>
2024-03-09 22:46:23 +0300

    Обновлена модель User

--- config/settings.py
+++ config/settings.py
@@ -125,3 +125,13 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
--- users/models.py
+++ users/models.py
@@ -3,4 +3,14 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    first_name = models.CharField(max_length=150, verbose_name='имя', blank=True)
+    last_name = models.CharField(max_length=150, verbose_name='фамилия', blank=True)
+    surname = models.CharField(max_length=150, verbose_name='отчество',  blank=True)
+    email = models.EmailField(unique=True, verbose_name='email')
+
+    def __str__(self):
+        return f' {self.first_name} {self.last_name} {self.surname} {self.email}'
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
