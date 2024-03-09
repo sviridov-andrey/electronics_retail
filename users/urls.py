c96f77d77cef302b0b71f90ffaba5af2473b318f
Андрей <chosenfew@yandex.ru>
2024-03-09 23:54:52 +0300

    Создан urls модели User

--- users/urls.py
+++ users/urls.py
@@ -1,15 +1,12 @@
-from django.urls import path
-# from rest_framework_simplejwt.views import (
-#     TokenObtainPairView,
-#     TokenRefreshView,
-# )
+from django.urls import path, include
+from rest_framework import routers
+from .views import UserViewSet
 
-from users.apps import UsersConfig
 
-app_name = UsersConfig.name
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
 
 urlpatterns = [
-    # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
+    path('', include(router.urls)),
 ]
