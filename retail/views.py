0109a0b209b3189edc2a0f3efd9cd6ec3f18b780
Андрей <chosenfew@yandex.ru>
2024-03-10 21:46:37 +0300

    Созданы вьюсеты сети

--- retail/views.py
+++ retail/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import filters
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Factory, Network, Entrepreneur, Product
+from .serializers import FactorySerializer, NetworkSerializer, EntrepreneurSerializer, ProductSerializer
+
+
+class FactoryViewSet(viewsets.ModelViewSet):
+    queryset = Factory.objects.all()
+    serializer_class = FactorySerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('country',)
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return Response(serializer.data)
+
+
+class NetworkViewSet(viewsets.ModelViewSet):
+    queryset = Network.objects.all()
+    serializer_class = NetworkSerializer
+    
+
+class EntrepreneurViewSet(viewsets.ModelViewSet):
+    queryset = Entrepreneur.objects.all()
+    serializer_class = EntrepreneurSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
