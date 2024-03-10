67941e8e2c9874b020183dfdb72918a9bc4897d0
Андрей <chosenfew@yandex.ru>
2024-03-10 23:22:05 +0300

    Созданы модели продуктов

---
+++ products/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from retail.models import Factory, Network, Entrepreneur
+
+
+class BaseProduct(models.Model):
+    prod_name = models.CharField(max_length=150, verbose_name='название продукта')
+    model = models.CharField(max_length=150, verbose_name='модель')
+    product_launch_date = models.DateTimeField(verbose_name='дата выхода на рынок')
+
+    class Meta:
+        abstract = True
+
+
+class FactoryProduct(BaseProduct):
+    factory = models.ManyToOneRel(Factory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f' {self.prod_name}'
+
+    class Meta:
+        verbose_name = 'продукт завода'
+        verbose_name_plural = 'продукты завода'
+
+
+class NetworkProduct(BaseProduct):
+    network = models.ManyToOneRel(Network, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f' {self.prod_name}'
+
+    class Meta:
+        verbose_name = 'продукт розничной сети'
+        verbose_name_plural = 'продукты розничных сетей'
+
+
+class EntrepreneurProduct(BaseProduct):
+    entrepreneur = models.ManyToOneRel(Entrepreneur, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f' {self.prod_name}'
+
+    class Meta:
+        verbose_name = 'продукт индивидуального предпринимателя'
+        verbose_name_plural = 'продукты индивидуальных предпринимателей'
