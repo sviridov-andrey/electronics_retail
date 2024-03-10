8dd17ac96144bd78f1a153d32027f12b9ceb8080
Андрей <chosenfew@yandex.ru>
2024-03-11 00:16:44 +0300

    Создана админка продуктов

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from products.models import FactoryProduct, NetworkProduct, EntrepreneurProduct
 
-# Register your models here.
+
+@admin.register(FactoryProduct)
+class FactoryProductAdmin(admin.ModelAdmin):
+    list_display = '__all__'
+
+
+@admin.register(NetworkProduct)
+class NetworkProductAdmin(admin.ModelAdmin):
+    list_display = '__all__'
+
+
+@admin.register(EntrepreneurProduct)
+class EntrepreneurProductAdmin(admin.ModelAdmin):
+    list_display = '__all__'
