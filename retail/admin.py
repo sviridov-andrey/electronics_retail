5058fed164fe4e3bf5d3c3f852e72f6f7edfff4a
Андрей <chosenfew@yandex.ru>
2024-03-10 19:53:12 +0300

    Созданы зарегистрированы в админке

--- retail/admin.py
+++ retail/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Factory, Network, Product, Entrepreneur
 
-# Register your models here.
+
+class ProductInline(admin.TabularInline):
+    model = Product
+
+
+class FactoryAdmin(admin.ModelAdmin):
+    inlines = [ProductInline]
+
+
+class NetworkAdmin(admin.ModelAdmin):
+    inlines = [ProductInline]
+
+
+class EntrepreneurAdmin(admin.ModelAdmin):
+    inlines = [ProductInline]
+
+
+admin.site.register(Factory, FactoryAdmin)
+admin.site.register(Network, NetworkAdmin)
+admin.site.register(Entrepreneur, EntrepreneurAdmin)
+admin.site.register(Product)
