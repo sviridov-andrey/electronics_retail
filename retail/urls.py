b06ab9b0d54f5398e19ec768014ba53a6b48fe8a
Андрей <chosenfew@yandex.ru>
2024-03-10 21:46:49 +0300

    Созданы роутеры сети

--- retail/urls.py
+++ retail/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path, include
 from rest_framework import routers
+from retail.views import FactoryViewSet, NetworkViewSet, EntrepreneurViewSet, ProductViewSet
 from users.apps import UsersConfig
 
 app_name = UsersConfig.name
 
 
 router = routers.DefaultRouter()
-# router.register(r'retail', RetailViewSet)
+router.register(r'factory', FactoryViewSet)
+router.register(r'network', NetworkViewSet)
+router.register(r'entrepreneur', EntrepreneurViewSet)
+router.register(r'product', ProductViewSet)
 
 
 urlpatterns = [
