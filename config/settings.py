2080dbcd2a8478281efff41d663c60d41daec66d
Андрей <chosenfew@yandex.ru>
2024-03-10 14:30:36 +0300

    Создано приложение retail

--- config/settings.py
+++ config/settings.py
@@ -85,8 +85,6 @@ DATABASES = {
         'NAME': os.getenv("POSTGRES_DB"),
         'USER': os.getenv("POSTGRES_USER"),
         'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
-        # 'HOST': os.getenv("POSTGRES_HOST"),
-        # 'PORT': os.getenv("POSTGRES_PORT"),
     }
 }
 
